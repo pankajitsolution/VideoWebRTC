001984a97e28a07b37c6759610d45016c7bcad61
pankajitsolution <124341501+pankajitsolution@users.noreply.github.com>
2023-05-31 17:39:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// index.js
+
+const http = require('http');
+const express = require('express');
+const { Server: SocketIO } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new SocketIO(server);
+// create a users map to keep track of users
+const users = new Map();
+
+io.on('connection', socket => {
+    console.log(`user connected: ${socket.id}`);
+    users.set(socket.id, socket.id);
+
+    // emit that a new user has joined as soon as someone joins
+    socket.emit('user:joined', socket.id);
+
+    socket.on('outgoing:call', data => {
+        const { fromOffer, to } = data;
+
+        socket.to(to).emit('incomming:call', { from: socket.id, offer: fromOffer });
+    });
+
+    socket.on('call:accepted', data => {
+        const { answere, to } = data;
+        socket.to(to).emit('incomming:call', { from: socket.id, offer: answere })
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log(`user disconnected: ${socket.id}`);
+        users.delete(socket.id);
+    });
+});
+
+
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static( path.resolve('./public') ));
+
+app.get('/users', (req, res) => {
+    return res.json(Array.from(users));
+});
+
+server.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
